414b601268ae3df9d586729388f6bcfe64b82720
Armando Sarza <asarza.pro97@gmail.com>
2025-11-30 22:25:52 -0800

    End of video 2: Wire up display View bindings Scrolling view. It's beautiful!

--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -1,24 +1,77 @@
 package com.example.gymlog;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gymlog.databinding.ActivityMainBinding;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private static final String TAG = "DAC_GYMLOG";
+    String mExercise = "";
+    double mWeight = 0.0;
+    int mReps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // Allows us to reference our information.
+        setContentView(binding.getRoot());
+
+        // Allows us to scroll through the log display screen
+        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        // This handles the LOG button function
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInformationFromDisplay();    // Get information first
+                updateDisplay();                // Update and output onto display
+            }
         });
+    }
+
+    /**
+     * This method allows us to update the information
+     * we retrieve from the user to the display.
+     */
+    private void updateDisplay(){
+        // Retrieve value from "Hello World"
+        String currentInfo = binding.logDisplayTextView.getText().toString();
+        Log.d(TAG, "current info: "+currentInfo);
+        String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s", mExercise, mWeight, mReps, currentInfo);
+        binding.logDisplayTextView.setText(newDisplay);
+    }
+
+    /**
+     * This method grabs the input information entered on the lines of
+     * Exercise, Weights and Reps and placed them in our global variables.
+     */
+    private void getInformationFromDisplay(){
+        // Take value from the user input line of "Exercise"
+        mExercise = binding.exerciseInputEditText.getText().toString();
+        // Take value from input line of Weight (Need to parse to Double)
+        try {
+            mWeight = Double.parseDouble(binding.weightInputEditText.getText().toString());
+        }catch (NumberFormatException e){
+            Log.d(TAG, "Error reading value from Weight edit text.");
+        }
+        // Take value from input line of Weight (Need to parse to Integer)
+        try {
+            mReps = Integer.parseInt(binding.repInputEditText.getText().toString());
+        }catch (NumberFormatException e){
+            Log.d(TAG, "Error reading value from Reps edit text.");
+        }
+
+
     }
 }
